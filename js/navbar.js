4fed0d9f30d314eb33c3247fd70be9f5baeef814
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2024-02-01 00:08:41 -0500

    Datos demograficos

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,6 @@
+window.onload = function() {
+    var elemento = document.getElementById("ret")
+    console.log(elemento.clientWidth);
+    document.insertRule("body { background-color: lightblue; }", 0);
+  };
 
