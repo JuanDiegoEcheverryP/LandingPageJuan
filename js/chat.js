845366ddadd1524b91b10db69305543db37a82bb
JuanDiegoEcheverryP <juan_echeverry@javeriana.edu.co>
2024-02-07 23:16:43 -0500

    se corrige el chat

--- js/chat.js
+++ js/chat.js
@@ -1,25 +1,43 @@
+//Mensajes guardados
 let mensajes = []
 
+//Resize de la caja de texto de un mensaje
+function OnInput() {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + "px";
+}
+
+
+//Poner en un fragmento un mensaje
 function msg() {
+  let msg_user = document.querySelector("#chat-bar-user").value
   let msg_new = document.querySelector("#chat-bar-entrada").value
+  
   if(msg_new) {
     mensajes.push(msg_new)
   }
   
-  let fragment = ''
-  
-  for(a of mensajes) {
-    fragment += '<div class="mensaje-box">'
-    fragment += '<p class="mensaje-user">You</p>'
-    fragment += '<p class="mensaje-content">'+a+'</p>'
-    fragment += '</div>'
+  let fragment = document.querySelector("#chat-message-box").innerHTML
+  if(mensajes.length == 1) {
+    fragment = "";
   }
+  fragment += '<div class="mensaje-box">'
+  fragment += '<p class="mensaje-user">'+msg_user+'</p>'
+  fragment += '<textarea readonly class="mensaje-content">'+msg_new+'</textarea>'
+  fragment += '</div>'
   console.log(fragment)
   document.querySelector("#chat-message-box").innerHTML = fragment;
   
   document.querySelector("#chat-bar-entrada").value = '';
+
+  const tx = document.getElementsByTagName("textarea");
+  for (let i = 0; i < tx.length; i++) {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    tx[i].addEventListener("input", OnInput, false);
+  }
 }
 
+//Abrir-cerrar-chat
 let estadoChat = false;
 function cerrarChat() {
     if(estadoChat) {
